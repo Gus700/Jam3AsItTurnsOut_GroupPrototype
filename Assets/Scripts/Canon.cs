176e6e8f864368d1e40ed12199d1fb7fd6430d42
Justin Murillo <murillo_justin@yahoo.com>
2022-12-02 04:40:36 -0800

    added collision detection for projectiles

--- Assets/Scripts/Canon.cs
+++ Assets/Scripts/Canon.cs
@@ -13,10 +13,12 @@ public class Canon : MonoBehaviour
 
     public Unit_Manager um;
     public int mouseButton;
+    private Transform parent;
 
     private void Start()
     {
         um = GetComponentInParent<Unit_Manager>();
+        parent = transform.parent;
     }
 
     // Update is called once per frame
@@ -29,7 +31,7 @@ public class Canon : MonoBehaviour
             if (Input.GetMouseButtonDown(mouseButton))
             {
                 // creat an instance of the bullet 
-                Rigidbody2D bulletInstance = Instantiate(bullet, launchOffSet.position, weapon.rotation);
+                Rigidbody2D bulletInstance = Instantiate(bullet, launchOffSet.position, weapon.rotation, parent.transform.parent);
                 Vector2 lookDirect = mousePos - (Vector2)weapon.position;
                 // add the directional position of the mouse cursor to the velocity of the buller rb
                 bulletInstance.velocity = new Vector2(lookDirect.x * heavyShotShootForce, lookDirect.y * heavyShotShootForce);
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private Player_Manager pm;
+
+    private void Start()
+    {
+        pm = transform.parent.GetComponent<Player_Manager>();
+    }
+
+    private void Update()
+    {
+        Vector3 position = transform.position;
+        if (position.y < -25)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collider)
+    {
+        if (collider.gameObject.CompareTag("Unit") &&
+            collider.transform.parent != transform.parent
+        )
+        {
+            pm.score += 10;
+        }
+
+        Destroy(gameObject);
+    }
+}
